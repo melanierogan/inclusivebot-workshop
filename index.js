cda13a0875ef7520245c95f2c90ec05ac05d1451
Melanie <melanie.rogan@ft.com>
2021-05-05 15:27:46 +0100

    first lot of files for trying out actions

---
+++ index.js
@@ -0,0 +1,51 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const unfriendlyWords = [
+	'whitelist',
+	'blacklist',
+	'simply',
+	'master',
+	'slave',
+	'just',
+	'guys',
+	'obviously',
+	'sane',
+];
+
+try {
+	const token = core.getInput('github_token');
+	const closePermissionCheck = core.getInput('close_permission');
+	const message = core.getInput('message');
+	const octokit = new github.GitHub(token);
+	const { repo, payload } = github.context;
+	let body;
+	let closePermission;
+
+	if (closePermissionCheck === 'true') {
+		closePermission = true;
+	} else if (closePermissionCheck === 'false') {
+		closePermission = false;
+	} else {
+		closePermission = false;
+	}
+
+	if (payload && payload.pull_request && payload.pull_request.body) {
+		console.log(payload.pull_request, '<<< the pull request');
+		console.log(payload.pull_request.body, '<<< the pull request body');
+
+		if (body !== payload.issue.body) {
+			octokit.issues
+				.createComment({
+					owner: repo.owner,
+					repo: repo.repo,
+					issue_number: payload.issue.number,
+					body: message,
+				})
+				.catch(e => {
+					throw e;
+				});
+		}
+	}
+} catch (error) {
+	core.setFailed(error.message);
+}
